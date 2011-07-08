4396d7324a35e0b97a7a7a31f9bf5db1adf2d9a5
Richard Shepherd <mail@richardshepherd.com>
2011-07-08 13:17:12 +0100

    Ignore the video folder cos its too big :)

--- js/script.js
+++ js/script.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
 	$window = $(window);
 	
 	// Cache the Y offset and the speed of each sprite
-	$('[data-controller]').each(function() {	
+	$('[data-type]').each(function() {	
 		$(this).data('offsetY', parseInt($(this).attr('data-offsetY')));
 		$(this).data('Xposition', $(this).attr('data-Xposition'));
 		$(this).data('speed', $(this).attr('data-speed'));
 	});
 	
-	// For each element that has a data-controller attribute
-	$('section[data-controller="background"]').each(function(){
+	// For each element that has a data-type attribute
+	$('section[data-type="background"]').each(function(){
 	
 	
 		// Store some variables based on where we are
@@ -52,7 +52,7 @@ $(document).ready(function(){
 				$self.css({ backgroundPosition: coords });
 				
 				// Check for other sprites in this section	
-				$('[data-controller="sprite"]', $self).each(function() {
+				$('[data-type="sprite"]', $self).each(function() {
 					
 					// Cache the sprite
 					$sprite = $(this);
@@ -64,37 +64,25 @@ $(document).ready(function(){
 					$sprite.css({ backgroundPosition: coords });													
 					
 				}); // sprites
-						// Check for any Videos that need scrolling
-			$('[data-controller="video"]', $self).each(function() {
-				
-				// Cache the sprite
-				$video = $(this);
-				
-				// Use the same calculation to work out how far to scroll the sprite
-				var yPos = -($window.scrollTop() / $video.data('speed'));					
-				var coords = (yPos + $video.data('offsetY')) + 'px';
-
-				$video.css({ top: coords });													
-				
-			}); // video	
+			
+				// Check for any Videos that need scrolling
+				$('[data-type="video"]', $self).each(function() {
+					
+					// Cache the sprite
+					$video = $(this);
+					
+					// Use the same calculation to work out how far to scroll the sprite
+					var yPos = -($window.scrollTop() / $video.data('speed'));					
+					var coords = (yPos + $video.data('offsetY')) + 'px';
+	
+					$video.css({ top: coords });													
+					
+				}); // video	
+			
 			}; // in view
-
-			// Check for any Videos that need scrolling
-			$('[data-controller="video"]', $self).each(function() {
-				
-				// Cache the sprite
-				$video = $(this);
-				
-				// Use the same calculation to work out how far to scroll the sprite
-				var yPos = -($window.scrollTop() / $video.data('speed'));					
-				var coords = (yPos + $video.data('offsetY')) + 'px';
-
-				$video.css({ top: coords });													
-				
-			}); // video	
 	
 		}); // window scroll
 			
-	});	// each data-controller
+	});	// each data-type
 
 }); // document ready
